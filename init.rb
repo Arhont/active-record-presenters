aa741c5b1f110e398908aae94dbabadc3366ef3f
Edward Tsech <edtsech@gmail.com>
2011-07-11 18:29:10 +0700

    Changes in init

--- init.rb
+++ init.rb
@@ -1,7 +1,5 @@
-ActiveSupport::Dependencies.load_paths << File.join(Rails.root, 'app', 'presenters')
-
-require 'presenter'
-require 'active_record_extension'
+require 'lib/presenter'
+require 'lib/presenter/active_record_extension'
 
 class << ActionController::Base
   def add_template_helper_with_presenters(helper_module, *args, &block)
@@ -9,6 +7,6 @@ class << ActionController::Base
     Presenter::HelperProxy.instance_eval { include helper_module }
     add_template_helper_without_presenters(helper_module, *args, &block)
   end
-  
+
   alias_method_chain :add_template_helper, :presenters
 end
